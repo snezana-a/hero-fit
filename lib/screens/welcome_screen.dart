c78465a82cdd8f24844d644a8df321153c9145e6
Snezana <snezanaanastasova99@gmail.com>
2023-08-29 23:22:40 +0200

    Add welcome screen

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset(
+            'assets/background_welcome.jpg',
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/logo.png',
+                  width: 150,
+                  height: 150,
+                ),
+                const SizedBox(height: 16),
+                const Text(
+                  'Continue by signing up, or continue with your existing account',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 32),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // Log In Button
+                    ElevatedButton(
+                      onPressed: () {
+                        // login logic
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                      ),
+                      child: const Text(
+                        'Log In',
+                        style: TextStyle(color: Colors.lightBlueAccent),
+                      ),
+                    ),
+                    const SizedBox(width: 16),
+
+                    ElevatedButton(
+                      onPressed: () {
+                        // registration logic
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                      ),
+                      child: const Text(
+                        'Register',
+                        style: TextStyle(color: Colors.lightBlueAccent),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
